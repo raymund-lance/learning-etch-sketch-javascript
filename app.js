d4e5846b026ce405acc9e3256b3dcf1ad09c3a23
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-19 15:26:40 +0800

    add conditions so mouseover only works when mousedown

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const selectContainer = document.querySelector('#container');
-for(let i = 0; i < 100; i++) {
+for(let i = 0; i < 150; i++) {
     const createChildContainer = document.createElement('div');
     createChildContainer.classList.add('horizontal');
     selectContainer.appendChild(createChildContainer);
@@ -8,20 +8,30 @@ for(let i = 0; i < 100; i++) {
 var selectHorizontal = document.querySelectorAll('.horizontal');
 var createDescendant = document.createElement('div');
 
-for(let i = 0; i < 100; i++) {
+for(let i = 0; i < 150; i++) {
     selectHorizontal.forEach(function(element) {
         createDescendant.classList.add('vertical'); 
         var clone = createDescendant.cloneNode(true);
         element.appendChild(clone);
     });
 }
-
+let isMouseDown = false;
 var selectVertical = document.querySelectorAll('.vertical');
 var backGroundColor = 'black';
 selectVertical.forEach(function(hover) {
+    hover.addEventListener('mousedown', () => {
+        isMouseDown = true;
+    });
+
     hover.addEventListener('mouseover', () => {
-        hover.style.backgroundColor = backGroundColor;
-    })
+        if(isMouseDown) {
+            hover.style.backgroundColor = backGroundColor;
+        }
+    });
+    
+    hover.addEventListener('mouseup', () => {
+        isMouseDown = false;
+    });
 })
 
 
