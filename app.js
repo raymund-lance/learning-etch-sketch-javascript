1c2bdc8745e097badef5bc0390ed7e746e05be27
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-19 15:15:38 +0800

    add change background color on hover for each vertical div

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const selectContainer = document.querySelector('#container');
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < 100; i++) {
     const createChildContainer = document.createElement('div');
     createChildContainer.classList.add('horizontal');
     selectContainer.appendChild(createChildContainer);
@@ -8,11 +8,21 @@ for(let i = 0; i < 16; i++) {
 var selectHorizontal = document.querySelectorAll('.horizontal');
 var createDescendant = document.createElement('div');
 
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < 100; i++) {
     selectHorizontal.forEach(function(element) {
         createDescendant.classList.add('vertical'); 
         var clone = createDescendant.cloneNode(true);
         element.appendChild(clone);
     });
 }
+
+var selectVertical = document.querySelectorAll('.vertical');
+var backGroundColor = 'black';
+selectVertical.forEach(function(hover) {
+    hover.addEventListener('mouseover', () => {
+        hover.style.backgroundColor = backGroundColor;
+    })
+})
+
+
 
