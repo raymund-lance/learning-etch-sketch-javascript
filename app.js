9b33aaa2f4634642b0dafd9d3bf549c5337a2378
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-19 15:37:09 +0800

    add title and button that will reset sketch pad

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const selectContainer = document.querySelector('#container');
-for(let i = 0; i < 150; i++) {
+for(let i = 0; i < 120; i++) {
     const createChildContainer = document.createElement('div');
     createChildContainer.classList.add('horizontal');
     selectContainer.appendChild(createChildContainer);
 }
 
+const selectSketch = document.querySelector('#sketch');
+selectSketch.addEventListener('click', () => {
+    selectVertical.forEach(function(newBoard) {
+        newBoard.style.backgroundColor = 'white';
+    })
+})
+
+
 var selectHorizontal = document.querySelectorAll('.horizontal');
 var createDescendant = document.createElement('div');
 
-for(let i = 0; i < 150; i++) {
+for(let i = 0; i < 120; i++) {
     selectHorizontal.forEach(function(element) {
         createDescendant.classList.add('vertical'); 
         var clone = createDescendant.cloneNode(true);
