d031463bc4e64008591399536112b6911ef5edc0
Lance <gonzalesraymundlance0729@gmail.com>
2024-07-19 16:23:51 +0800

    Add colors on header so the paint can be change colors

--- app.js
+++ app.js
@@ -25,7 +25,55 @@ for(let i = 0; i < 120; i++) {
 }
 let isMouseDown = false;
 var selectVertical = document.querySelectorAll('.vertical');
-var backGroundColor = 'black';
+
+var colorPaint = 'black';
+
+
+const colorBlack = document.querySelector('#black')
+colorBlack.addEventListener('click', () => {
+    colorPaint = 'black';
+})
+
+const colorRed = document.querySelector('#red')
+colorRed.addEventListener('click', () => {
+    colorPaint = 'red';
+})
+
+const colorGreen = document.querySelector('#green')
+colorGreen.addEventListener('click', () => {
+    colorPaint = 'green';
+})
+
+const colorYellow = document.querySelector('#yellow')
+colorYellow.addEventListener('click', () => {
+    colorPaint = 'yellow';
+})
+
+const colorOrange = document.querySelector('#orange')
+colorOrange.addEventListener('click', () => {
+    colorPaint = 'orange';
+})
+
+const colorBlue = document.querySelector('#blue')
+colorBlue.addEventListener('click', () => {
+    colorPaint = 'blue';
+})
+
+const colorPurple = document.querySelector('#purple')
+colorPurple.addEventListener('click', () => {
+    colorPaint = 'purple';
+})
+
+const colorPink = document.querySelector('#pink')
+colorPink.addEventListener('click', () => {
+    colorPaint = 'pink';
+})
+
+const colorWhite = document.querySelector('#white')
+colorWhite.addEventListener('click', () => {
+    colorPaint = 'white';
+})
+
 selectVertical.forEach(function(hover) {
     hover.addEventListener('mousedown', () => {
         isMouseDown = true;
@@ -33,7 +81,7 @@ selectVertical.forEach(function(hover) {
 
     hover.addEventListener('mouseover', () => {
         if(isMouseDown) {
-            hover.style.backgroundColor = backGroundColor;
+            hover.style.backgroundColor = colorPaint;
         }
     });
     
